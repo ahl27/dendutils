013187e40e2030a59cefe5eb98076ef1bf97afe0
ahl27 <ahl27@pitt.edu>
2024-04-28 15:44:57 -0400

    fixes bug where duplicate labels cause issues

--- R/labels.R
+++ R/labels.R
@@ -8,10 +8,28 @@
   }
 
   l <- labels(object)
-  names(value) <- l
-  dendrapply(object, \(x){
-    if(is.leaf(x))
-      attr(x, 'label') <- value[[attr(x,'label')]]
-    x
-  }, how='post.order')
+  ll <- length(l)
+  if(ll != length(value)){
+    warning("Number of items to replace not equal to replacement length")
+    value <- rep(value, length.out=ll)
+  }
+
+  # faster replacement if all are unique
+  if(length(unique(l)) == ll){
+    names(value) <- l
+    dendrapply(object, \(x){
+      if(is.leaf(x))
+        attr(x, 'label') <- value[[attr(x,'label')]]
+      x
+    }, how='post.order')
+  } else {
+    .INTERNAL_LABEL_CTR <- 1L
+    dendrapply(object, \(x){
+      if(is.leaf(x)){
+        attr(x, 'label') <- value[.INTERNAL_LABEL_CTR]
+        .INTERNAL_LABEL_CTR <<- .INTERNAL_LABEL_CTR+1L
+      }
+      x
+    }, how='post.order')
+  }
 }
